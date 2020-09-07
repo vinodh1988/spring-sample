f3332aed0d3b0f0f440f129b762fa4bd0338443f
vinodh1988 <k.c.vinodh@hotmail.com>
2020-09-07 20:20:13 +0530

    new one now

--- src/main/java/com/firstservice/control/FirstController.java
+++ src/main/java/com/firstservice/control/FirstController.java
@@ -10,7 +10,7 @@ public class FirstController {
 	String n[]= {"James","Sharan","Hari"};
   @GetMapping("/greet")
   public String greet() {
-	  return "Hi!!! Spring boot is running";
+	  return "Hi!!! Spring boot is running , Doing well";
   }
   
   @GetMapping("/greet2")
